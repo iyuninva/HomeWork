758379ad287f74e4c6c6a5dfafaa604f842978d0
IyuninVA <IyuninVA@bk.ru>
2022-12-20 19:30:14 +0400

    add task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Добрый день! Задача №13");
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+String c = "-";
+String b = Convert.ToString(a);
+if (b[0]==c[0])
+{
+	Console.WriteLine(b[3]);
+}
+else
+{
+	Console.WriteLine(b[2]);
+}
+
