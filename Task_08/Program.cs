43e454a60d7f5cf6c18005a3b221e5d28c7e5e4b
IyuninVA <IyuninVA@bk.ru>
2022-12-20 19:26:08 +0400

    add task_10

---
+++ Task_08/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Добрый день! Задача №8");
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+while (count < a)
+{
+    count++;
+    Console.WriteLine(count+2);
+}
