7d35a023cce6b373ee781b37b6350b725c25386e
IyuninVA <IyuninVA@bk.ru>
2022-12-26 21:57:08 +0400

    add Task_21

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+// 1 Вариант ----------------------------------------------------------------------
+// Console.WriteLine("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number >= 10000 && number <= 99999 || number >= -99999 && number <= -10000)
+// {
+//     int a1 = number / 10000;
+//     int b1 = number % 10;
+//     if (a1 == b1)
+//     {
+//         int a2 = number / 1000 % 10;
+//         int b2 = number % 100 / 10;
+//         if (a2 == b2)
+//         {
+//             Console.WriteLine($"Число {number} является палиндромом");
+//         }
+//         else
+//         {
+//             Console.WriteLine($"Число {number} не является палиндромом!");
+//         } 
+//     }
+//     else
+//         {
+//             Console.WriteLine($"Число {number} не является палиндромом!");
+//         }
+// }
+// else
+// {
+//     Console.WriteLine("Error - Введите пятизначное число!");
+// }
+//---------------------------------------------------------------------------------
+// 2 Вариант ----------------------------------------------------------------------
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 10000 && number <= 99999 || number >= -99999 && number <= -10000)
+{
+    String a = Convert.ToString(number);
+    if (a.Contains("-")==true && a[1] == a[5] && a[2] == a[4])
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else if (a[0] == a[4] && a[1] == a[3])
+    {
+        Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {number} не является палиндромом!");
+    }
+}
+else
+{
+    Console.WriteLine("Error - Введите пятизначное число!");
+}
+//---------------------------------------------------------------------------------
---
+++ Task_21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координату X1: ");
+double X1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y1: ");
+double Y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z1: ");
+double Z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X2: ");
+double X2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y2: ");
+double Y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Z2: ");
+double Z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
+
+Console.WriteLine($"Расстояние между точками ({X1}, {Y1}, {Z1}) и ({X2}, {Y2}, {Z2}) = {Math.Round(d, 2)}");
