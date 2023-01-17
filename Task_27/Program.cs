82c788a749e111d9ba9dae42c68ea7291b680134
IyuninVA <IyuninVA@bk.ru>
2023-01-17 22:40:59 +0400

    add Task_29

---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int[] ArraySum(int[] array)
+{
+    
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = number[i];
+    }
+    return array;
+}
+
+Console.WriteLine("Введите число: ");
+int numberInt = Convert.ToInt32(Console.ReadLine());
+string number = Convert.ToString(numberInt);
+int[] array = new int[number.Length];
+Console.WriteLine(ArraySum(array));
---
+++ Task_29/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] ArrayRandom(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-999, 1000);
+    }
+    return array;
+}
+
+int[] array = new int[8];
+ArrayRandom(array);
+Console.Write("Массив из 8 элементов: ");
+Console.WriteLine(string.Join(", ", array));
+
+Console.WriteLine();
+Console.Write("Нажмите Enter для завершения программы ");
+Console.ReadLine();
