86444f2d5adb30d8b4862a12026cca9045a86440
IyuninVA <IyuninVA@bk.ru>
2023-01-17 19:23:45 +0400

    add Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B. (без использования Math.Pow)
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double DegreeMethod(int A, int B)
+{
+    int degree = 1;
+    for(int i = 0; i < B || -i > B; i++)
+    {   
+        degree *= A;
+    }
+    return degree;
+}
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double degree = DegreeMethod(A, B);
+if (B > 0)
+{
+    Console.WriteLine();
+    Console.WriteLine($"Результатом возведения числа {A} в степень {B} является: {degree}");  
+}
+else
+{
+    double NegativeDegree = 1/degree;
+    Console.WriteLine();
+    Console.WriteLine($"Результатом возведения числа {A} в степень {B} является: {NegativeDegree}");  
+}
+
+Console.WriteLine();
+Console.Write("Нажмите Enter для завершения программы ");
+Console.ReadLine();
+
+
