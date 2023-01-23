9eb01baed6022f5b145f8c787e2e4bef07b3d584
IyuninVA <IyuninVA@bk.ru>
2023-01-24 03:26:32 +0400

    add Task_43

---
+++ Task_41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Метод заполнения массива:
+void ArrayNumber(int Arraylength)
+{
+    int[] Array = new int[Arraylength];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Console.Write($"Введите {i+1} число: ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Индекс №{i}: [" + string.Join(", ", Array) + "]");
+    }
+    Sum(Array);
+}
+
+// Метод вычесления суммы больше 0: 
+void Sum(int[] Array)
+{
+    int sum = 0;
+    for (int j = 0; j < Array.Length; j++)
+    {
+
+        if (Array[j] > 0)
+        {
+            sum++;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Введено чисел больше 0: [{sum}]");
+}
+
+// Ввод с клавиатуры длины массива:
+Console.Write("Нажмите Enter для запуска программы: ");
+Console.ReadLine();
+Console.Write("Введите длину массива: ");
+int Arraylength = Convert.ToInt32(Console.ReadLine());
+ArrayNumber(Arraylength);
+
+// Окончание программы:
+Console.Write("Нажмите Enter для завершения программы: ");
+Console.ReadLine();
+
+
+
---
+++ Task_43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void Straight(double b1, double k1, double b2, double k2)
+{
+    double x = (b1 - b2) / (k2 - k1);
+    double y = (k2 * b1 - k1 * b2) / (k2 - k1);
+    if (k1 == k2)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Заданные прямые не пересекаются!");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine($"Точка пересечения заданных прямых: ({x}; {y})");
+    }
+}
+
+// Ввод данных:
+Console.Write("Нажмите Enter для запуска программы: ");
+Console.ReadLine();
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Straight(b1, k1, b2, k2);
+
+// Окончание программы:
+Console.Write("Нажмите Enter для завершения программы: ");
+Console.ReadLine();
+
+
