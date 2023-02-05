d976502801600be681b3d65fb5035a2143a6a947
IyuninVA <IyuninVA@bk.ru>
2023-02-06 00:23:31 +0400

    end Task_68

--- Task_68/Program.cs
+++ Task_68/Program.cs
@@ -3,7 +3,7 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 // Метод подсчета функции Аккермана
-int PrintAckerman(int NumberOne, int NumberTwo)
+int Ackerman(int NumberOne, int NumberTwo)
 {
     if (NumberOne == 0)
     {
@@ -11,34 +11,13 @@ int PrintAckerman(int NumberOne, int NumberTwo)
     }
     else if (NumberOne > 0 && NumberTwo == 0)
     {
-        return PrintAckerman(NumberOne - 1, 1);
+        return Ackerman(NumberOne - 1, 1);
     }
     else if (NumberOne > 0 && NumberTwo > 0)
     {
-        return PrintAckerman(NumberOne - 1, PrintAckerman(NumberOne, NumberTwo - 1));
+        return Ackerman(NumberOne - 1, Ackerman(NumberOne, NumberTwo - 1));
     }
-    return PrintAckerman(NumberOne, NumberTwo);
-}
-
-// Метод проверки положительного числа
-int PositiveNumber(int NumberOne, int NumberTwo)
-{
-    if (NumberOne < 0 || NumberTwo < 0)
-    {
-        Console.WriteLine("Введите положительные натуральные числа!");
-        return Input(NumberOne, NumberTwo);
-    }
-    return PrintAckerman(NumberOne, NumberTwo);
-}
-
-// Метод ввода чисел
-int Input(int NumberOne, int NumberTwo)
-{
-    Console.Write("Введите число №1: ");
-    NumberOne = Convert.ToInt32(Console.ReadLine());
-    Console.Write("Введите число №2: ");
-    NumberTwo = Convert.ToInt32(Console.ReadLine());
-    return PositiveNumber(NumberOne, NumberTwo);
+    return Ackerman(NumberOne, NumberTwo);
 }
 
 // Старт программы
@@ -46,14 +25,16 @@ Console.WriteLine("Задача №68");
 Console.Write("Нажмите Entrer для запуска программы: ");
 Console.ReadLine();
 Console.WriteLine();
-int NumberOne = 0;
-int NumberTwo = 0;
-Input(NumberOne, NumberTwo);
 
-// Результат работы программы и вывод в консоль
+Console.Write("Введите число №1: ");
+int NumberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число №2: ");
+int NumberTwo = Convert.ToInt32(Console.ReadLine());
+Ackerman(NumberOne, NumberTwo);
+
 Console.WriteLine();
 Console.Write("Результат работы программы: ");
-Console.WriteLine(PrintAckerman(NumberOne, NumberTwo));
+Console.WriteLine(Ackerman(NumberOne, NumberTwo));
 
 // Завершенеи программы
 Console.WriteLine();
